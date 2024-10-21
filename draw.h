8a4bfd84c5ccd891c51fb538fcc8ecf15d5c7f8d
Pablo <pablo.ferreira@efrei.net>
2024-10-21 18:11:50 +0200

    added beta of draw.c/.h

---
+++ draw.h
@@ -0,0 +1,19 @@
+//
+// Created by pablo on 21/10/2024.
+//
+
+#ifndef DRAW_H
+#define DRAW_H
+
+//function to return a list of 9 moves following a probability rule and using the structure t_moves in moves.h
+
+#include "moves.h"
+
+/**
+ * @brief function to draw 9 moves
+ * @param moves : the pointer to the structure of moves
+ * @return the list of 9 moves
+ */
+char draw9Moves();
+
+#endif //DRAW_H
